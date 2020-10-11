40f59b51c98a28a7c43e45a22095032fddc4b3d7
prahlad <prahladskingdom@hotmail.com>
2020-10-11 21:47:26 +0530

    Stable Commit 3

--- Angular/knowlad-ionic/src/app/shared/constants/knowload.constants.ts
+++ Angular/knowlad-ionic/src/app/shared/constants/knowload.constants.ts
@@ -1,4 +1,4 @@
-// export const API_URL = 'http://localhost:9000';
-export const API_URL = 'http://52.66.46.3:9000';
+export const API_URL = 'http://localhost:9000';
+// export const API_URL = 'http://52.66.46.3:9000';
 export const API_KEY = 'njTFtTortOfGr067CXcN7L8HDplYrUdqmsRe';
 export const API_SECRET = 'cjx2Apa9NC3T2hskh7VTEJN91Jbp9QtfHOWA';
